affe923ad7cacd610b7d895036f0cfb809461153
jiehoes <folurians@gmail.com>
2026-04-17 23:36:17 +0700

    my

--- webhook.py
+++ webhook.py
@@ -1,13 +1,23 @@
 from flask import Flask, request, abort
 import subprocess
+import hmac
+import hashlib
 
 app = Flask(__name__)
 
-SECRET = "mysecret123"
+SECRET = b"mysecret123"
 
 @app.route('/deploy', methods=['POST'])
 def deploy():
-    if request.headers.get("X-Secret") != SECRET:
+    signature = request.headers.get('X-Hub-Signature-256')
+    if not signature:
+        abort(403)
+
+    sha_name, signature = signature.split('=')
+
+    mac = hmac.new(SECRET, msg=request.data, digestmod=hashlib.sha256)
+
+    if not hmac.compare_digest(mac.hexdigest(), signature):
         abort(403)
 
     subprocess.Popen(['/home/jiehoes/geonode-project/deploy.sh'])
