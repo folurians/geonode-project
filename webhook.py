a7ca833be88a161782f806133e6c59fdf7b1c3ef
jiehoes <folurians@gmail.com>
2026-04-18 17:33:08 +0700

    edit webhook

--- webhook.py
+++ webhook.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/deploy', methods=['POST'])
 def deploy():
-    subprocess.Popen(['/home/jiehoes/geonode-project/deploy.sh'])
-    return 'Deploy triggered', 200
+    subprocess.Popen(
+        ['/home/jiehoes/geonode-project/deploy.sh'],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000)
